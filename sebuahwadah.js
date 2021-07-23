7637b90b7714a49575c91fcd8e6cfaca4e595dc0
Andrian Cedric <andriancedric@gmail.com>
2021-07-23 10:46:28 +0700

    New portfolio website

---
+++ sebuahwadah.js
@@ -0,0 +1,17 @@
+// document ready
+(function ($) {
+
+    // scroll functions
+    $(window).scroll(function(e) {
+    
+        // add/remove class to navbar when scrolling to hide/show
+        var scroll = $(window).scrollTop();
+        if (scroll >= 150) {
+            $('.navbar').addClass("navbar-hide");
+        } else {
+            $('.navbar').removeClass("navbar-hide");
+        }
+    
+    });
+    
+})(jQuery); 
