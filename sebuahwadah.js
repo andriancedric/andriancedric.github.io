df5e31e06a6a4e40856355a589a1867ce3a08fae
Andrian Cedric <andriancedric@gmail.com>
2021-09-08 20:48:41 +0700

    Add BCA Mobile

--- sebuahwadah.js
+++ sebuahwadah.js
@@ -7,7 +7,7 @@
         // add/remove class to navbar when scrolling to hide/show
         var scroll = $(window).scrollTop();
         if (scroll >= 150) {
-            $('.navbar').addClass("navbar-hide");
+            $('.avbar').addClass("navbar-hide");
         } else {
             $('.navbar').removeClass("navbar-hide");
         }
