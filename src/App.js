648ebe63828e4583a69feab66cce210b6cf63670
Andrian Cedric <andrian.cedric@moonlay.com>
2024-02-08 16:43:03 +0700

    update router into HashRouter and deploy

--- src/App.js
+++ src/App.js
@@ -8,18 +8,20 @@ import Experience from "./components/Experience"
 import Certificate from "./components/Certificate"
 import Contact from "./components/Contact"
 import Footer from './components/Footer';
-import { HashRouter as Router, Routes, Route}
+import { HashRouter as Routes, Route}
     from 'react-router-dom';
 
 export default function App() {
   return (
     <>
-    <Navbar /><Routes>
+    <Navbar />
+    <Routes>
       <Route exact path='/' element={<Home />} />
       <Route path='/portfolio' element={<Portfolio />} />
       <Route path='/experience' element={<Experience />} />
       <Route path='/certificate' element={<Certificate />} />
       <Route path='/contact' element={<Contact />} />
-    </Routes><Footer /></>
+    </Routes>
+    <Footer /></>
   );
 }
